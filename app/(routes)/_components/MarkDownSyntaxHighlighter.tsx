91eb16a96def8e248d33fad3a891f78391ca98bf
gokuljs <gokuljs029@gmail.com>
2024-03-10 16:35:50 +0530

    recusive code

--- app/(routes)/_components/MarkDownSyntaxHighlighter.tsx
+++ app/(routes)/_components/MarkDownSyntaxHighlighter.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactMarkdown from "react-markdown";
 import rehypeHighlight from "rehype-highlight";
 import "highlight.js/styles/github.css";
+import { useTheme } from "next-themes";
 
 interface MarkDownSyntaxHighlighterProps {
   content: string;
@@ -9,7 +10,16 @@ interface MarkDownSyntaxHighlighterProps {
 const MarkDownSyntaxHighlighter: React.FC<MarkDownSyntaxHighlighterProps> = ({
   content,
 }) => {
+  const { theme } = useTheme();
+  React.useEffect(() => {
+    if (theme === "light") {
+      require("highlight.js/styles/darcula.css");
+    } else {
+      require("highlight.js/styles/github.css");
+    }
+  }, [theme]);
   if (!content || content.length === 0) return;
+
   return (
     <ReactMarkdown rehypePlugins={[rehypeHighlight]}>{content}</ReactMarkdown>
   );

d936df43106f0ceb9ed45c9392f931ee8d903792
gokuljs <gokuljs029@gmail.com>
2024-03-10 11:29:03 +0530

    chat-label

---
+++ app/(routes)/_components/chatLabel.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ChatLabel = () => {
+  return (
+    <div className="w-[400px] h-[200px] border flex flex-col items-center justify-center">
+      <p>How can I help you today?</p>
+    </div>
+  );
+};
+
+export default ChatLabel;

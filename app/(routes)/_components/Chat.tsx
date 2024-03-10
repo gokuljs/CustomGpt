df41e7b4c2c3c9ee9c7eac0049a1c4ee4268cf2c
gokuljs <gokuljs029@gmail.com>
2024-03-10 16:48:53 +0530

    chat messages

--- app/(routes)/_components/Chat.tsx
+++ app/(routes)/_components/Chat.tsx
@@ -1,6 +1,7 @@
 import OpenAI from "openai";
 import React from "react";
 import MarkDownSyntaxHighlighter from "./MarkDownSyntaxHighlighter";
+import Logo from "@/components/logo";
 
 export interface ChatModelProps {
   message: OpenAI.Chat.ChatCompletionMessageParam[];
@@ -12,10 +13,22 @@ const Chat: React.FC<ChatModelProps> = ({ message, isLoading }) => {
     <div className="h-full w-full flex justify-center items-center">
       <div className="h-full lg:w-[80%] w-[100%] px-10 py-10 flex flex-col gap-4  h-[calc(100% - 80px)]">
         {message.map((item, index) => (
-          <MarkDownSyntaxHighlighter
-            key={index}
-            content={item?.content ? (item.content as string) : ""}
-          />
+          <div key={index} className="flex justify-start items-start gap-2">
+            <div className="h-[40px] w-[40px]">
+              {item.role === "assistant" && (
+                <div className="rounded-full dark:bg-white bg-violet-200 flex items-center justify-center h-7 w-7">
+                  <Logo width={20} height={20} />
+                </div>
+              )}
+            </div>
+            <div className="flex flex-col gap-3">
+              <div>{item.role}</div>
+              <MarkDownSyntaxHighlighter
+                key={index}
+                content={item?.content ? (item.content as string) : ""}
+              />
+            </div>
+          </div>
         ))}
       </div>
     </div>

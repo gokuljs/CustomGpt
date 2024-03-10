e8ad32dfd6266201e707ce363244cb25a44c4361
gokuljs <gokuljs029@gmail.com>
2024-03-10 20:14:39 +0530

    small fix

--- app/(routes)/layout.tsx
+++ app/(routes)/layout.tsx
@@ -7,7 +7,7 @@ interface HomePageProps {
 
 const layout: React.FC<HomePageProps> = ({ children }) => {
   return (
-    <div className="h-full w-full flex">
+    <div className="h-[100vh] w-[100vw] flex">
       <aside className="px-3 py-3 mobile lg:block hidden">
         <ModeToggle />
       </aside>

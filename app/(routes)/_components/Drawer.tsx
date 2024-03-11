5ddc01a6f8b67ef6b5ce49178b6cf40308487ca2
gokuljs <gokuljs029@gmail.com>
2024-03-11 06:03:44 +0530

    clear history feature

--- app/(routes)/_components/Drawer.tsx
+++ app/(routes)/_components/Drawer.tsx
@@ -6,14 +6,38 @@ import { Button } from "@/components/ui/button";
 import { Menu } from "lucide-react";
 import useGetAllMessages, { MessageProps } from "@/lib/useGetAllMessagesData";
 import { Avatar, AvatarFallback, AvatarImage } from "@radix-ui/react-avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { toast } from "sonner";
 
 const History = () => {
   const data: MessageProps[] = useGetAllMessages();
+
   const [sideBarActive, setSideBarActive] = useState<boolean>(false);
-  const allMessages = data
-    .reverse()
-    .map((item) => ({ id: item.id, content: item.messages[0].content }));
-  console.log(allMessages);
+  const allMessages =
+    data.length > 0
+      ? data
+          .reverse()
+          .map((item) => ({ id: item.id, content: item.messages[0].content }))
+      : [];
+
+  function deleteItemById() {
+    const localStorageKey = process.env.LOCAL_STORAGE_KEY || "defaultKey";
+    // Check if the item exists in local storage
+    if (localStorage.getItem(localStorageKey) !== null) {
+      // Delete the item
+      localStorage.removeItem(localStorageKey);
+      toast("Item with ID ${localStorageKey} has been deleted.");
+    } else {
+      toast("Item with ID ${localStorageKey} does not exist.");
+    }
+  }
 
   return (
     <>
@@ -26,13 +50,23 @@ const History = () => {
         `}
       >
         <div className="h-[60px] flex justify-between items-center px-2">
-          <Avatar className="h-8 w-8 rounded-full">
-            <AvatarImage
-              className="object-cover object-center rounded-full"
-              src="/profileThree.jpeg"
-            />
-            <AvatarFallback>CN</AvatarFallback>
-          </Avatar>
+          <DropdownMenu>
+            <DropdownMenuTrigger>
+              <Avatar className="h-8 w-8 rounded-full">
+                <AvatarImage
+                  className="object-cover object-center h-8 w-8 rounded-full"
+                  src="/profileThree.jpeg"
+                />
+                <AvatarFallback>CN</AvatarFallback>
+              </Avatar>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="start">
+              <DropdownMenuItem onClick={() => deleteItemById()}>
+                Clear History
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+
           <Button
             className="rounded-full h-7 w-7 px-1 py-1 hover:bg-zinc-800 hover:text-white"
             variant={"ghost"}
@@ -47,8 +81,17 @@ const History = () => {
               className="px-2 py-2 cursor-pointer rounded dark:hover:bg-zinc-800 hover:bg-zinc-800 hover:text-white flex items-center justify-between"
               key={item.id}
             >
-              {item.content ? (item.content as string) : ""}
-              <EllipsisVertical />
+              <div className="max-w-xs overflow-hidden whitespace-nowrap text-overflow-ellipsis">
+                {item.content ? (item.content as string) : ""}
+              </div>
+              <DropdownMenu>
+                <DropdownMenuTrigger>
+                  <EllipsisVertical />
+                </DropdownMenuTrigger>
+                <DropdownMenuContent align="start">
+                  <DropdownMenuItem>Delete</DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
             </div>
           ))}
         </div>
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/provider/themeProvider";
+import { Toaster } from "@/components/ui/sonner";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -27,6 +28,7 @@ export default function RootLayout({
           {children}
         </ThemeProvider>
       </body>
+      <Toaster />
     </html>
   );
 }
